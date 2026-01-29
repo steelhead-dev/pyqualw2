898daf925358d586fe330b592651685630f2d2c3
pdmurray <peynmurray@gmail.com>
2026-01-29 10:35:46 -0800

    Add pre-commit hooks to the project

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ We have ported CE-QUAL-W2 to Linux under the name of CE-QUAL-W2
 
 Preprocessor.py takes in a directory of Qual-W2 model files and intializes the
 process for model runs
+
+## Development
+
+To set up your development environment, install the `dev` optional dependency
+group:
+
+```bash
+pip install -e '.[dev]'
+```
+
+This will install the package along with some other developer tooling.
+
+### Pre-commit hooks
+
+This project uses pre-commit hooks to ensure code quality. To get started,
+ensure [`prek`](https://github.com/j178/prek) is installed (it's one of the
+dependencies included in the optional `dev` dependency group). Then run
+
+```bash
+prek install
+```
+
+to install the pre-commit hooks. Read more about pre-commit hooks on the [`prek`
+github page](https://github.com/j178/prek).
--- pyqualw2/preprocess.py
+++ pyqualw2/preprocess.py
@@ -1,60 +1,50 @@
-import typer
 from pathlib import Path
-from typing_extensions import Annotated
-import pandas as pd
-import os
+from typing import Annotated
+
 import tomli
+import typer
 
 app = typer.Typer()
 
-model_directory =""
+model_directory = ""
 
 model_file_dictionary = {}
+
 
 @app.command()
 def process_directory(
     directory: Annotated[
         Path,
         typer.Argument(
-            help='The directory to process.',
-            exists=True, #Ensure diectory exisit
-            dir_okay=True, # Ensure it's a directory
-            file_okay=False, # Ensure it's not a file
-        )
-    ]
+            help="The directory to process.",
+            exists=True,  # Ensure diectory exisit
+            dir_okay=True,  # Ensure it's a directory
+            file_okay=False,  # Ensure it's not a file
+        ),
+    ],
 ):
-    """
-    Enter in directory containg Ce-QUAL-W2 model files
-    """
-
+    """Enter in directory containg Ce-QUAL-W2 model files."""
     model_directory = directory.resolve()
-    print(f"Absolute path of the directory: {model_directory}")
 
     for item in model_directory.iterdir():
         if item.name == "inputs":
             model_file_dictionary["model_inputs_path"] = item.resolve()
-#            print(f"Config file logged at {model_file_dictionary["model_inputs_path"]}")
         if item.name == "outputs":
             model_file_dictionary["model_outputs_path"] = item.resolve()
-#            print(f"Config file logged at {model_file_dictionary["model_outputs_path"]}")
         if item.name == "w2_con.csv":
             model_file_dictionary["model_config_path"] = item.resolve()
-#            print(f"Config file logged at {model_file_dictionary["model_config_path"]}")
         if item.name == "run_settings.toml":
             model_file_dictionary["run_settings_path"] = item.resolve()
-#            print(f"Config file logged at {model_file_dictionary["run_settings_path"]}")
 
-#def parse_run_settings ():
-    with open(model_file_dictionary["run_settings_path"], mode = 'rb') as f:
+    with open(model_file_dictionary["run_settings_path"], mode="rb") as f:
         settings = tomli.load(f)
-        #print(settings)
 
-        model_details = settings['model_details']
-        run_settings = settings['run_settings']
-        time_start = run_settings['time_start']
-        time_end = run_settings['time_end']
-        print(f"The starting date and tiem is {time_start}")
-        print(f"The ending date and tiem is {time_end}")
+        run_settings = settings["run_settings"]
+        time_start = run_settings["time_start"]
+        time_end = run_settings["time_end"]
+        print(f"The starting date and tiem is {time_start}")  # noqa: T201
+        print(f"The ending date and tiem is {time_end}")  # noqa: T201
+
 
 if __name__ == "__main__":
     app()
